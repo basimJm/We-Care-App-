69c8219555ee5fd6dd29a99bcc2e5f0cfc1f78b2
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2023-02-09 15:22:16 -0500

    Logout dialog added on logout

--- lib/viewModel/home_vm.dart
+++ lib/viewModel/home_vm.dart
@@ -3,10 +3,32 @@ import 'package:mapd722_group2_project/constants/route_name.dart';
 
 class HomeVM extends ChangeNotifier {
   onLogout({required BuildContext context}) {
-    Navigator.pushNamedAndRemoveUntil(
-      context,
-      RoutesName.loginRoute,
-      (route) => false,
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Logout'),
+          content: const Text('Are you sure you want to logout ?'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('No'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamedAndRemoveUntil(
+                  context,
+                  RoutesName.loginRoute,
+                  (route) => false,
+                );
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
