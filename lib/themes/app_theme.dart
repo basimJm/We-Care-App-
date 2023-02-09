30cd6f3da925e86694837cc9107aefbd69742f23
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2023-02-09 14:08:40 -0500

    adjust app theme

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 // App icon color
 const Color kIconColor = Colors.white;
-const Color kPrimaryColor = Color(0xFF514F8C);
+const Color kPrimaryColor = Color(0xFF2E9B95);
 const Color kTextColor = Colors.black;
 const Color kHomeBottomBarColor = Color(0xFF962E3A);
 const Color kTabBarBackgroundColor = Color.fromARGB(255, 165, 24, 24);
 
-const Color kButtonColor = Color(0xFF3F3E6D);
+const Color kButtonColor = Color(0xFF2A948E);
 
 // Setup App global theme
 ThemeData appTheme = ThemeData(
